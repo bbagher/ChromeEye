71c1e94e5a1fa1544dec17fd61493c56a7138447
BB <bjbagher@gmail.com>
2020-02-04 23:45:05 -0500

    fixed bugs

--- popup.js
+++ popup.js
@@ -1,115 +1,105 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  
-  let activate = document.getElementById('checkbox');
-;
-  chrome.storage.local.get(['activate'], function(result) {
-    let activateRes = result.activate
-    if (activateRes) activate.checked = activateRes 
-    else activate.checked = false
-       });
-  chrome.storage.local.get(['size'], function(result) {
-    let sizeRes = result.size
-    if (sizeRes) sizeStep.value = sizeRes
-    else sizeStep.value = 50
-       }); 
-  chrome.storage.local.get(['speed'], function(result) {
-      let speedRes = result.speed
-      if (speedRes) speedStep.value = speedRes 
-       });
+window.addEventListener("DOMContentLoaded", event => {
+  let activate = document.getElementById("checkbox");
+  let speedStep = document.getElementById("speed");
+  let sizeStep = document.getElementById("size");
 
+  const retrieveFromLocals = (key, node) => {
+    chrome.storage.local.get([key], function(result) {
+      let res = result[key];
+      if (res) node.value = res;
+    });
+  };
+
+
+  const setToLocals = (key, value) => {
+    chrome.storage.local.set({ [key]: value }, function() {
+      console.log("Value is set to " + value);
+    });
+  };
+
+  retrieveFromLocals("size", sizeStep);
+  retrieveFromLocals("speed", speedStep);
 
   const chromeEye = (speed, size) => {
-    const readEye = (text) => { 
-      let actualSpeed = Number(speed) * 10
-      let actualSize = 2 * Number(size)
-      console.log('actual size ', actualSize)
-      console.log('actual speed ', actualSpeed)
-      console.log(actualSpeed)
-      let og = text.innerHTML.split(' ').slice().join(' ')
-      let textArray = text.innerText.split(' ');
-      console.log(textArray);
+    const readEye = text => {
+      let actualSpeed = Number(speed) * 10;
+      let actualSize = Number(size) * 2;
+      console.log("actual size ", actualSize);
+      console.log("actual speed ", actualSpeed);
+      console.log(actualSpeed);
+      let og = text.innerHTML
+        .split(" ")
+        .slice()
+        .join(" ");
+      let textArray = text.innerText.split(" ");
       let offset = 0;
       let temp = textArray[0];
-      
-      
+
       for (let i = 0; i < textArray.length; i++) {
         timeout = setTimeout(() => {
-          text.innerHTML = textArray.join(' ');
+          text.innerHTML = textArray.join(" ");
           temp = textArray[i];
-          textArray[i] = `<mark style="background-color: yellow; font-size: ${actualSize}px;"><strong>${textArray[i]}</strong></mark>`;
-          text.innerHTML = textArray.join(' ');
-          console.log(textArray)
+          textArray[
+            i
+          ] = `<center><mark style="background-color: pink; font-size: ${actualSize}px;"><strong>${textArray[i]}</strong></mark></center>`;
+          text.innerHTML = textArray.join(" ");
           textArray[i] = temp;
         }, 0 + offset);
         offset += actualSpeed;
-        
       }
-      setTimeout(() => text.innerHTML = og, offset)
+      timeout2 = setTimeout(() => (text.innerHTML = og), offset);
     };
-    
-    
-    
-    document.addEventListener("click", read = () => {
-      readEye(event.toElement)
-    })
-    
-    
-  }
-  console.log('DOM fully loaded and parsed');
-  
-  let speedStep = document.getElementById('speed');
-  let sizeStep = document.getElementById('size');
-    
-  let speed = 50
-  let size = 50;
-  let count = 0;
-    
-    speedStep.addEventListener("click", function(eventIn) {
-      speed = eventIn.toElement.valueAsNumber
-      chrome.storage.local.set({speed: speed}, function() {
-        console.log('Value is set to ' + speed);
-      });  
-    })
-    
-    
-    sizeStep.addEventListener("click", function(eventIn) {
-      size = eventIn.toElement.valueAsNumber    
-      chrome.storage.local.set({size: size}, function() {
-        console.log('Value is set to ' + size);
+
+    document.addEventListener(
+      "click",
+      (read = () => {
+        readEye(event.toElement);
+      })
+    );
+  };
+  console.log("DOM fully loaded and parsed");
+
+
+  speedStep.addEventListener("click", function(eventIn) {
+    speed = eventIn.toElement.valueAsNumber;
+    setToLocals("speed", speed);
+  });
+
+  // make size persistent by setting it in store after change
+  sizeStep.addEventListener("click", function(eventIn) {
+    size = eventIn.toElement.valueAsNumber;
+    setToLocals("size", size);
+  });
+
+  activate.addEventListener("click", async function(eventIn) {
+ 
+    await chrome.tabs.query({ active: true, currentWindow: true }, function(
+      tabs
+    ) {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: 'if (read) document.removeEventListener("click",read)'
       });
-    })
-      
-      activate.addEventListener("click", function(eventIn) {
-      count += 1;        
-      console.log(count)
-      if (count % 2 !== 0) {
+    });
 
-        chrome.storage.local.set({activate: true}, function() {
-          console.log('Value is set to ' + true);
-        });
-          
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.executeScript(
-              tabs[0].id,
-              {code : '('+chromeEye+')('+speed+','+size+')'}
-              );
-            })
-            
-          }
-          else {
-            
-
-        chrome.storage.local.set({activate: false}, function() {
-          console.log('Value is set to ' + false);
-        });
-              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                  chrome.tabs.executeScript(
-                tabs[0].id,
-                {code : 'if (read) document.removeEventListener("click",read)'}
-                );
-              })
-            }
-          
-          }    
-          )
-        });
+    await chrome.tabs.query({ active: true, currentWindow: true }, function(
+      tabs
+    ) {
+      console.log(
+        "activating script at ",
+        speedStep.value,
+        "and size ",
+        sizeStep.value
+      );
+      chrome.tabs.executeScript(tabs[0].id, {
+        code:
+          "(" + chromeEye + ")(" + speedStep.value + "," + sizeStep.value + ")"
+      });
+    });
+  });
+  
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: 'if (read) document.removeEventListener("click",read)'
+    });
+  });
+});
