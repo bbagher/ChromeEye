7c7ad9d8935ae9377abd976b8b8125bff284e20a
BB <bjbagher@gmail.com>
2020-02-04 19:57:17 -0500

    mvp

--- popup.js
+++ popup.js
@@ -1,37 +1,115 @@
-let speed = 2500;
-// speed = prompt("What speed would you like to read?");
-console.log("user input speed", speed);
-let color = "yellow";
-// color = prompt("What is your favorite color?");
-let fontsize = "100px";
-// fontsize = prompt("How large would you like your text?");
-let timeout;
-
-const chromeEye = text => { 
-  let og = text.innerHTML.split(' ').slice().join(' ')
-  let textArray = text.innerText.split(' ');
-  console.log(textArray);
-  let offset = 0;
-  let temp = textArray[0];
+window.addEventListener('DOMContentLoaded', (event) => {
   
+  let activate = document.getElementById('checkbox');
+;
+  chrome.storage.local.get(['activate'], function(result) {
+    let activateRes = result.activate
+    if (activateRes) activate.checked = activateRes 
+    else activate.checked = false
+       });
+  chrome.storage.local.get(['size'], function(result) {
+    let sizeRes = result.size
+    if (sizeRes) sizeStep.value = sizeRes
+    else sizeStep.value = 50
+       }); 
+  chrome.storage.local.get(['speed'], function(result) {
+      let speedRes = result.speed
+      if (speedRes) speedStep.value = speedRes 
+       });
 
-  for (let i = 0; i < textArray.length; i++) {
-    timeout = setTimeout(() => {
-      text.innerHTML = textArray.join(' ');
-      temp = textArray[i];
-      textArray[i] = `<mark style="background-color: ${color}; font-size: ${fontsize}px;"><strong>${textArray[i]}</strong></mark>`;
-      text.innerHTML = textArray.join(' ');
-      console.log(textArray)
-      textArray[i] = temp;
-    }, 0 + offset);
-    offset += 500;
-  
+
+  const chromeEye = (speed, size) => {
+    const readEye = (text) => { 
+      let actualSpeed = Number(speed) * 10
+      let actualSize = 2 * Number(size)
+      console.log('actual size ', actualSize)
+      console.log('actual speed ', actualSpeed)
+      console.log(actualSpeed)
+      let og = text.innerHTML.split(' ').slice().join(' ')
+      let textArray = text.innerText.split(' ');
+      console.log(textArray);
+      let offset = 0;
+      let temp = textArray[0];
+      
+      
+      for (let i = 0; i < textArray.length; i++) {
+        timeout = setTimeout(() => {
+          text.innerHTML = textArray.join(' ');
+          temp = textArray[i];
+          textArray[i] = `<mark style="background-color: yellow; font-size: ${actualSize}px;"><strong>${textArray[i]}</strong></mark>`;
+          text.innerHTML = textArray.join(' ');
+          console.log(textArray)
+          textArray[i] = temp;
+        }, 0 + offset);
+        offset += actualSpeed;
+        
+      }
+      setTimeout(() => text.innerHTML = og, offset)
+    };
+    
+    
+    
+    document.addEventListener("click", read = () => {
+      readEye(event.toElement)
+    })
+    
+    
   }
-  setTimeout(() => text.innerHTML = og, offset)
-};
+  console.log('DOM fully loaded and parsed');
+  
+  let speedStep = document.getElementById('speed');
+  let sizeStep = document.getElementById('size');
+    
+  let speed = 50
+  let size = 50;
+  let count = 0;
+    
+    speedStep.addEventListener("click", function(eventIn) {
+      speed = eventIn.toElement.valueAsNumber
+      chrome.storage.local.set({speed: speed}, function() {
+        console.log('Value is set to ' + speed);
+      });  
+    })
+    
+    
+    sizeStep.addEventListener("click", function(eventIn) {
+      size = eventIn.toElement.valueAsNumber    
+      chrome.storage.local.set({size: size}, function() {
+        console.log('Value is set to ' + size);
+      });
+    })
+      
+      activate.addEventListener("click", function(eventIn) {
+      count += 1;        
+      console.log(count)
+      if (count % 2 !== 0) {
 
-document.addEventListener("click", async () => {
-  console.log(event.toElement.textContent);
+        chrome.storage.local.set({activate: true}, function() {
+          console.log('Value is set to ' + true);
+        });
+          
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(
+              tabs[0].id,
+              {code : '('+chromeEye+')('+speed+','+size+')'}
+              );
+            })
+            
+          }
+          else {
+            
 
-  chromeEye(event.toElement)
-});
+        chrome.storage.local.set({activate: false}, function() {
+          console.log('Value is set to ' + false);
+        });
+              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                  chrome.tabs.executeScript(
+                tabs[0].id,
+                {code : 'if (read) document.removeEventListener("click",read)'}
+                );
+              })
+            }
+          
+          }    
+          )
+        });
